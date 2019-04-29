51f41bbc633f820469019f067ec137d3d85587fc
northcheng <northcheng@me.com>
2019-04-29 14:57:09 +0800

    update

--- bc_tiger.py
+++ bc_tiger.py
@@ -1,4 +1,6 @@
+
 # 获取用户账户信息
-def get_user_info(info_path):
+def get_user_info(info_path='drive/My Drive/tiger_quant/'):
   user_info = pd.read_csv(info_path + 'user_info.csv')
   return user_info.astype('str').loc[0,:].to_dict()
+
