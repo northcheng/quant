2171523b0e67c8d83608088a01ac721446de4bfd
northcheng <northcheng@me.com>
2019-06-18 14:48:32 +0800

    Update bc_machine_learning.py

--- bc_machine_learning.py
+++ bc_machine_learning.py
@@ -53,13 +53,14 @@ def get_train_test_data(scaled_data, input_dim, output_dim, test_size=0.1, is_sh
 
     print(2)
     # 预测数据
-    # if len(predict_idx) > 0: 
     predict_idx = [util.string_2_time(x) for x in predict_idx]
     predict_data = scaled_data.loc[predict_idx, :].copy()
     predict_x = predict_data[input_dim].values.reshape(-1, len(input_dim))
     predict_y = predict_data[output_dim].values.reshape(-1, len(output_dim))
 
+    print(2.5)
     # 从训练数据中删除预测数据
+    if len(predict_idx) > 0:
     for idx in predict_idx:
       train_data.drop(idx, inplace=True)
     
