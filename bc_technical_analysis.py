f667b15d2ff4ac2e48886e96ab3459f2ba45f006
northcheng <northcheng@me.com>
2019-07-18 11:37:06 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -391,7 +391,7 @@ def cal_ichimoku_signal(df):
   
 
 # Plot Ichimoku
-def plot_ichimoku(data, start=None, end=None, signal_dim=None, title=None):
+def plot_ichimoku(data, start=None, end=None, signal_dim=None, title=None, save_path=None):
   
     plot_data = data[start:end]
   
@@ -417,6 +417,10 @@ def plot_ichimoku(data, start=None, end=None, signal_dim=None, title=None):
   
     plt.legend()  
     plt.title(title)
+
+    if save_path is not None:
+        plt.savefig(save_path + title + '.png')
+        plt.show()
 
 
 #----------------------------- 技术指标可视化 -----------------------------------#
