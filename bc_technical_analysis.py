e0777a2e1037ee4eaefca284564f9e5efa2b0069
northcheng <northcheng@me.com>
2019-08-12 17:48:52 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1181,47 +1181,6 @@ def plot_indicator(df, target_col, price_col='Close', start=None, end=None, benc
     return ax
 
 
-def plot_ichimoku_and_mr(df, std_multiple=2, start=None, end=None, title=None, save_path=None, show_image=False):
-  """
-  Plot Ichimoku and mean reversion in a same plot
-
-  :param df: dataframe with ichimoku and mean reversion columns
-  :param std_multiple: std_multiple for mean reversion
-  :param start: start of the data
-  :param end: end of the data
-  :param title: title of the figure
-  :param save_path: path where the figure will be saved to
-  :param use_ax: the already-created ax to draw on
-  :returns: plot
-  :raises: none
-  """
-  # select plot data
-  plot_data = df[start:end].copy()
-
-  # create figures
-  fig = plt.figure(figsize=(20, 6))
-  gs = gridspec.GridSpec(2, 1, height_ratios=[2, 1])
-
-  # Ichimoku plot
-  ichimoku_plot = plt.subplot(gs[0]) 
-  plot_ichimoku(df=plot_data, title=title, use_ax=ichimoku_plot)
-
-  # Mean reversion plot
-  mean_reversion_plot = plt.subplot(gs[1], sharex=ichimoku_plot) 
-  plot_mean_reversion(df=plot_data, std_multiple=std_multiple, use_ax=mean_reversion_plot)
-
-  # adjust plot layout
-  plt.tight_layout() 
-
-  # save image
-  if save_path is not None:
-    plt.savefig(save_path + title + '.png')
-    
-  # close image
-  if not show_image:
-    plt.close(fig)
-
-
 def plot_multiple_indicators(df, args={'name': ['ichimoku', 'mean_reversion'], 'mean_reversion': {'std_multiple': 2}}, start=None, end=None, title=None, save_path=None, show_image=False):
   """
   Plot Ichimoku and mean reversion in a same plot
@@ -1247,9 +1206,7 @@ def plot_multiple_indicators(df, args={'name': ['ichimoku', 'mean_reversion'], '
   num_indicators = len(indicators)
 
   # create figures
-  fig = plt.figure(figsize=(20, num_indicators*3))
-  fig.suptitle(title)
-  
+  fig = plt.figure(figsize=(20, num_indicators*3))  
   gs = gridspec.GridSpec(num_indicators, 1, height_ratios=list(np.ones(num_indicators,dtype=np.int8)))
 
   axes = {}
@@ -1276,7 +1233,8 @@ def plot_multiple_indicators(df, args={'name': ['ichimoku', 'mean_reversion'], '
 
   # adjust plot layout
   plt.tight_layout() 
-  
+  fig.suptitle(title, ha='left')
+
   # save image
   if save_path is not None:
     plt.savefig(save_path + title + '.png')
