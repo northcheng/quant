978758ef777f70b691b0ca601557785f3ce32cce
northcheng <northcheng@me.com>
2019-08-26 13:03:01 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -615,16 +615,16 @@ def add_adx_features(df, n=14, close='Close', open='Open', high='High', low='Low
   df = df.copy()
 
   print('0')
+  # calculate true range
+  df = add_atr_features(df=df, cal_signal=False)
+
+  print('1')
   # plus/minus directional movement
   df['high_diff'] = df[high] - df[high].shift(1)
   df['low_diff'] = df[low].shift(1) - df[low]
   df['zero'] = 0
   df['pdm'] = df['high_diff'].combine(df['zero'], lambda x1, x2: get_min_max(x1, x2, 'max'))
   df['mdm'] = df['low_diff'].combine(df['zero'], lambda x1, x2: get_min_max(x1, x2, 'max'))
-  
-  print('1')
-  # calculate true range
-  df = add_atr_features(df=df, cal_signal=False)
   
   print('2')
   # smooth directional movement, true range
@@ -1320,7 +1320,7 @@ def add_atr_features(df, n=14, close='Close', open='Open', high='High', low='Low
   if cal_signal:
     df['atr_signal'] = 'n'
 
-  df.drop(['h_l', 'h_pc', 'l_pc'], axis=1, inplace=True)
+  # df.drop(['h_l', 'h_pc', 'l_pc'], axis=1, inplace=True)
 
   return df
 
