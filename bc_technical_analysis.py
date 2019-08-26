4267b82b84493c9b374891e422997b2f38a615df
northcheng <northcheng@me.com>
2019-08-26 21:31:36 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1431,8 +1431,8 @@ def plot_peak_through(df, start=None, end=None, price_col='Close', signal_col='s
     ax = plt.gca()
 
   # get peaks and throughs
-  peaks = df.query('%(column)s != "%(value)s"' % dict(column=signal_col, value=pos_signal))
-  troughs = df.query('%(column)s != "%(value)s"' % dict(column=signal_col, value=neg_signal))
+  peaks = df.query('%(column)s == "%(value)s"' % dict(column=signal_col, value=pos_signal))
+  troughs = df.query('%(column)s == "%(value)s"' % dict(column=signal_col, value=neg_signal))
 
   # plot peak and through
   ax.plot(df.index, df[price_col])
