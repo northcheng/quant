215c64d776209b99e995bf1b322accc82d70996b
northcheng <northcheng@me.com>
2019-08-26 11:20:27 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1341,9 +1341,13 @@ def add_atr_features(df, n=14, close='Close', open='Open', high='High', low='Low
   
   idx = df.index.tolist()
   for i in range(n, len(df)):
-    current_idx = idx[i]
-    previous_idx = idx[i-1]
-    df.loc[current_idx, 'atr'] = (df[previous_idx, 'atr'] * 13 + df[current_idx, 'tr']) / 14
+    try:
+      current_idx = idx[i]
+      previous_idx = idx[i-1]
+      df.loc[current_idx, 'atr'] = (df[previous_idx, 'atr'] * 13 + df[current_idx, 'tr']) / 14
+    except Excception as e:
+      print(df)
+
 
   # fill na value
   if fillna:
