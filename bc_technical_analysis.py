01c9aa456ecb949f498e873a5fcff05e9f6a9406
northcheng <northcheng@me.com>
2019-09-10 09:35:05 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1726,7 +1726,7 @@ def plot_multiple_indicators(df, args={'plot_ratio': {'ichimoku':1.5, 'mean_reve
         title=tmp_indicator, use_ax=axes[tmp_indicator])
 
   # adjust plot layout
-  fig.suptitle(title, x=0.5, y=0.9)
+  fig.suptitle(title, x=0.5, y=0.9, fontsize=20)
 
   # save image
   if save_path is not None:
