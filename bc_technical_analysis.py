38e21e3bc3fb2d0a4c139f902238eb21c0b4098a
northcheng <northcheng@me.com>
2019-11-05 21:05:07 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1659,7 +1659,7 @@ def add_kama_features(df, n_param={'kama_fast': [10, 2, 30], 'kama_slow': [10, 5
   return df
 
 #* Money Flow Index(MFI)
-def add_mfi_features(df, n=14, close='Close', open='Open', high='High', low='Low', volume='Volume', fillna=False, cal_signal=True):
+def add_mfi_features(df, n=14, close='Close', open='Open', high='High', low='Low', volume='Volume', fillna=False, cal_signal=True, boundary=[20, 80]):
   """
   Calculate Money Flow Index Signal
 
@@ -1706,7 +1706,7 @@ def add_mfi_features(df, n=14, close='Close', open='Open', high='High', low='Low
 
   # calculate signals
   if cal_signal:
-    df['mfi_signal'] = 'n'
+    df['mfi_signal'] = cal_boundary_signal(df=df, upper_col='mfi', lower_col='mfi', upper_boundary=max(boundary), lower_boundary=min(boundary), result_col='signal', pos_signal='b', neg_signal='s', none_signal='n')
 
   df.drop('up_or_down', axis=1, inplace=True)
   return df
