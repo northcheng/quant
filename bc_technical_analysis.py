6f17c4fe18484515d1dc4309656b8b9d07ebe435
northcheng <northcheng@me.com>
2019-11-20 16:08:11 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -612,11 +612,13 @@ def add_adx_features(df, n=14, close='Close', open='Open', high='High', low='Low
 
     df = cal_change_rate(df=df, target_col='pdi', add_prefix=True)
     df = cal_change_rate(df=df, target_col='mdi', add_prefix=True)
-
+    print('a')
     pdi_periods = int(abs(df.iloc[-1]['pdi_acc_day']) + 1)
-    pdi_slope = linear_fit(df=df, target_col='pdi', periods=pdi_periods)[0]
     mdi_periods = int(abs(df.iloc[-1]['mdi_acc_day']) + 1)
+    print('b')
+    pdi_slope = linear_fit(df=df, target_col='pdi', periods=pdi_periods)[0]
     mdi_slope = linear_fit(df=df, target_col='mdi', periods=pdi_periods)[0]
+    print('c')
 
     df['pdi_slope'] = pdi_slope
     df['mdi_slope'] = mdi_slope
