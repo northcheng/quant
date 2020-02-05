a0c03fbb7040ee1201b07077788c7c5a4ea9d675
northcheng <northcheng@me.com>
2020-02-05 22:09:13 +0800

    Update bc_technical_analysis.py

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1268,32 +1268,29 @@ def add_ichimoku_features(df, n_short=9, n_medium=26, n_long=52, method='ta', is
       df['close_to_' + line] = round((df[close] - df[line]) / df[close], ndigits=3)
 
       # drop line signal columns
-      #col_to_drop.append(line_signal_name)
+      col_to_drop.append(line_signal_name)
       
     # ================================ Number days since breakthrough =================
-   # for line in ['tankan', 'kijun']:
-   #   df[f'{line}_day'] = df[f'signal_{line}'].replace({'b':1, 's':-1, 'n':0})
-   #   df[f'{line}_day'] = df[f'{line}_day'].astype(int)
-   # print(df[['tankan_day', 'kijun_day']].tail())
-   # print(type(df['tankan_day'].values[0]))
+    for line in ['tankan', 'kijun']:
+      df[f'{line}_day'] = df[f'signal_{line}'].copy()
     
-    #idx_list = df.index.tolist()
-    #for i in range(1, len(idx_list)):
-    #  current_idx = idx_list[i]
-    #  previous_idx = idx_list[i-1]
+    idx_list = df.index.tolist()
+    for i in range(1, len(idx_list)):
+      current_idx = idx_list[i]
+      previous_idx = idx_list[i-1]
 
       # go through each indicator
-    #  for indicator in ['tankan', 'kijun']:
-    #    day_col = f'{indicator}_day'
-    #    current_day = df.loc[current_idx, day_col]
-    #    previous_day = df.loc[previous_idx, day_col]
-    #    
-    #    # signal unchanged
-    #    if current_day == 0:
-    #      if previous_day > 0:
-    #        df.loc[current_idx, day_col] = previous_day + 1
-    #      elif previous_day < 0:
-    #        df.loc[current_idx, day_col] = previous_day - 1
+      for indicator in ['tankan', 'kijun']:
+        day_col = f'{indicator}_day'
+        current_day = df.loc[current_idx, day_col]
+        previous_day = df.loc[previous_idx, day_col]
+        
+        # signal unchanged
+        if current_day == 0:
+          if previous_day > 0:
+            df.loc[current_idx, day_col] = previous_day + 1
+          elif previous_day < 0:
+            df.loc[current_idx, day_col] = previous_day - 1
     
     # ================================ Signal =========================================
     if cal_signal:
