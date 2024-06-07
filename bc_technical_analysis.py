f63fe7ca6319294d38887499f13d551a89a59482
northcheng <northcheng@me.com>
2024-06-07 14:55:38 +0800

    update

--- bc_technical_analysis.py
+++ bc_technical_analysis.py
@@ -1405,8 +1405,8 @@ def calculate_ta_signal(df):
     '4':        '(3>= signal_day > 1)', 
     '3':        '(signal_day == 1)', 
     '2':        '(signal_day == 1) and ((adx_strong_day > 0) or (adx_direction_start < -10))', 
-    '1':        '(signal_day == 1) and (adx_strong_day > 0) and (adx_direction_start < -10)', 
-    '0':        '(完美_up == 1) and (signal_day == 1) and (adx_strong_day > 0) and (adx_direction_start < -10)', 
+    '1':        '(signal_day == 1) and (adx_strong_day > 0) and (adx_direction_start < -10) and (ichimoku_distance < 0 and 相对ichimoku位置 in ["mid_down", "mid"])', 
+    '0':        '(signal_day == 1) and (adx_strong_day > 0) and (adx_direction_start < -10) and (ichimoku_distance < 0 and 相对ichimoku位置 in ["down"])', 
   } 
   values = {
     '6':        6,
