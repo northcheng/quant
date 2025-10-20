0f5f342bbf3960b4651b19d61355d25f3a0b6cd3
northcheng <northcheng@me.com>
2025-10-20 14:26:23 +0800

    Update bc_data_io.py

--- bc_data_io.py
+++ bc_data_io.py
@@ -349,7 +349,8 @@ def get_data_from_ak(symbol: str, start_date: str = None, end_date: str = None, 
     if symbol.startswith('0') or symbol.startswith('3') or symbol.startswith('6'):
       result = ak.stock_zh_a_hist(symbol=symbol, period=interval, start_date=start_date, end_date=end_date, adjust=adjust)
     if symbol.startswith('15') or symbol.startswith('51') or symbol.startswith('56') or symbol.startswith('58'):
-      result = ak.fund_etf_hist_em(symbol=symbol, period=interval, start_date=start_date, end_date=end_date, adjust=adjust)
+      result = ak.stock_zh_a_hist(symbol=symbol, period=interval, start_date=start_date, end_date=end_date, adjust=adjust)
+      # result = ak.fund_etf_hist_em(symbol=symbol, period=interval, start_date=start_date, end_date=end_date, adjust=adjust)
   
   elif market == 'hk':
     result = ak.stock_hk_hist(symbol=symbol, period=interval, start_date=start_date, end_date=end_date, adjust=adjust)
